647440b0a36526cad0c63b688e2cc1d8e4880c2c
Leanne <leannestranks86@hotmail.com>
2020-11-12 21:56:30 +1000

    items model and controller with views created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,48 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:show, :update, :edit, :destroy]
+  before_action :set_appeals
+
   def index
+      @items = Item.all
   end
+
+  def new
+      @item = Item.new
+  end
+  
+  def create
+      item = Item.create!(item_params)
+      redirect_to item_path(item)
+  end
+  
+  def edit
+  end
+  
+  def update
+      @item.update(item_params)
+      redirect_to items_path
+  end
+  
+  def show
+  end
+
+  def destroy
+      @item.destroy
+      redirect_to items_path
+  end
+
+  private
+
+  def set_item
+      @item = Item.find(params[:item_id])
+  end
+
+  def set_appeals
+    @appeals = Appeal.all
+  end
+
+  def item_params
+      params.require(:item).permit(:description, :name, :price, :available, :image, appeal_ids: [])
+  end 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+    attribute :price, default: 2.0
     has_and_belongs_to_many :appeals
     has_one_attached :image
 end
