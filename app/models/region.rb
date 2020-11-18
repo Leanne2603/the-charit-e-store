9c95f58d83a47cbe4b5451ca7b7ac06c4bbf53cb
Leanne <leannestranks86@hotmail.com>
2020-11-18 20:11:34 +1000

    added in logic to ensure data not duplicated

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,7 @@
 class Region < ApplicationRecord
     has_many :appeals, dependent: :destroy
     validates :region, presence: true
+    validates :region, uniqueness: true
 
     self.per_page = 50
 end
